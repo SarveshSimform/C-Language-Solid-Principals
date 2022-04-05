506a03f63f44980e897d09af68eae7b589d36fe8
Sarvesh Patel <97871348+SarveshSimform@users.noreply.github.com>
2022-04-05 14:57:53 +0530

    Add files via upload

---
+++ Practical-6/Practical_6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Practical_6
+{
+    public delegate void Notify();
+    public class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted; // Event
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            // some code here..
+            OnProcessCompleted();
+        }
+
+        //protected virtual method
+        protected virtual void OnProcessCompleted() 
+        {
+            //if ProcessCompleted is not null then call delegate
+            ProcessCompleted?.Invoke();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Method Invoked");
+        }
+    }
+}
