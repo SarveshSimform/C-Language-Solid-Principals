cb81238b615fc143ac2f205ba3456cf1196f638e
Sarvesh Patel <97871348+SarveshSimform@users.noreply.github.com>
2022-04-05 11:36:26 +0530

    Add files via upload

---
+++ Practical_2/Practical_2/Customer_Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_2
+{
+    class Customer_Account
+    {
+        /// <summary>
+        /// Object initialize than Constructor called.
+        /// </summary>
+        /// <param name="customer_name"></param>
+        /// <param name="customer_accountNo"></param>
+        public Customer_Account(string customer_name, long customer_accountNo)
+        {
+            this.customer_name = customer_name;
+            this.customer_accountNo = customer_accountNo;
+        }
+        public string bank_name { get; set; }
+        public string customer_name { get; set; }
+        public long customer_accountNo { get; private set;}
+        
+        /// <summary>
+        /// return type is void nothing to return only print initialize value
+        /// </summary>
+        public void printInfo()
+        {
+            Console.WriteLine("****************************************************");
+            Console.WriteLine("Bank Name: "+bank_name);
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Customer Account No: "+customer_accountNo);
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Customer Name: "+customer_name);
+            Console.WriteLine("****************************************************");
+        }
+       
+    }
+}
---
+++ Practical_2/Practical_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practical_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object initialize and Constructor called automatically
+            Customer_Account customer_Account = new Customer_Account("Sarvesh Patel;", 215064114320);
+           
+            //assign customer bank name
+            customer_Account.bank_name = "State Bank of India";
+            
+            //called print method for display customer data
+            customer_Account.printInfo();
+        }
+    }
+}
