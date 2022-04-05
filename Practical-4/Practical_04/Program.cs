19552ad3bd4bdfe6b7056e5eb1695029ff5bea40
Sarvesh Patel <97871348+SarveshSimform@users.noreply.github.com>
2022-04-05 12:55:01 +0530

    Add files via upload

---
+++ Practical-4/Practical_04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Practical_04
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            //enter student name
+            Console.WriteLine("Enter Student Name");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Enter Marks");
+            
+            //loop will iterate until array length
+            for (int i=0; i<student.Marks.Length; i++)
+            {
+                //user enter marks
+                Console.WriteLine("Enter Subject No: " + (i+1));
+                student.Marks[i]= Convert.ToDecimal(Console.ReadLine());
+            }
+            
+            //this is average calculate function
+            var avg= student.CalculateAverageMarks();
+            Console.WriteLine("The Average is "+avg);
+            
+            //menu print 
+            Console.WriteLine(student.CalculateGrade(avg));
+
+        }
+    }
+}
