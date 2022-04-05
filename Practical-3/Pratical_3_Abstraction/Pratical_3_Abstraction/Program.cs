c3218a3250fc3a389dfe0b6654fd95227ae16641
Sarvesh Patel <97871348+SarveshSimform@users.noreply.github.com>
2022-04-05 12:35:56 +0530

    Add files via upload

---
+++ Practical-3/Pratical_3_Abstraction/Pratical_3_Abstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pratical_3_Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialized object
+            Laptop laptop = new Laptop();
+            //assign private variables value by setter
+            laptop.Brand = "Lenovo";
+            laptop.Model = "legion 5";
+            //print following output
+            laptop.LaptopDetails();
+        }
+    }
+}
